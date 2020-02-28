dd385d73752b606c920abd523df5688407bc4590
hutism <runandlove@gmail.com>
2020-02-28 11:50:28 +0900

    add Pipeline

---
+++ Pipeline.py
@@ -0,0 +1,20 @@
+'''
+As part of a data processing pipeline, complete the implementation of the pipeline method:
+
+The method should accept a variable number of functions, and it should return a new function that accepts one parameter arg.
+The returned function should call the first function in the pipeline with the parameter arg, and call the second function with the result of the first function.
+The returned function should continue calling each function in the pipeline in order, following the same pattern, and return the value from the last function.
+For example, pipeline(lambda x: x * 3, lambda x: x + 1, lambda x: x / 2) then calling the returned function with 3 should return 5.0.
+'''
+
+
+def pipeline(*funcs):
+    def helper(arg):
+        for i in range(len(funcs)):
+            arg = funcs[i](arg)
+        return arg
+    return helper
+
+
+fun = pipeline(lambda x: x * 3, lambda x: x + 1, lambda x: x / 2)
+print(fun(3))  # should print 5.0
